8149925702a4e552d303b13978373bc389b3239e
Shahzod Akhmedov <shahzod.akhmedov@bk.ru>
2022-01-06 14:46:01 +0500

    Добавьте файлы проекта.

---
+++ Chat service/Hubs/ChatHub.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chat_service.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly string _botUser;
+        private readonly IDictionary<string, UserConnection> _connection;
+
+        public ChatHub(IDictionary<string, UserConnection> connection)
+        {
+            _botUser = "MyChat Bot";
+            _connection = connection;
+        }
+        public async Task SendMessage(string Message)
+        {
+            if(_connection.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
+            {
+                await Clients.Group(userConnection.Room)
+                    .SendAsync("ReceiveMessage", userConnection.User, Message);
+            }
+        }
+
+        public async Task JoinRoom(UserConnection userConnection)
+        {
+            _connection[Context.ConnectionId] = userConnection;
+            await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
+            await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", _botUser, $"{userConnection.User} has joined {userConnection.Room}");
+        }
+
+    }
+}
